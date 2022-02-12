7662308b69269053e9b8e6c228e3f331cc590c03
qafoori <qafoori.hr@gmail.com>
2022-02-13 00:26:08 +0330

    maps & loops & conditions & mix

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@ const stylesSource = 'src/styles/**/*.scss'
 
 const compileStyles = () => src(stylesSource).pipe(sass()).pipe(dest('css'))
 
-const watchTask = () => {
-  watch([stylesSource], compileStyles)
-}
+const watchTask = () => watch([stylesSource], compileStyles)
 
 exports.default = series(compileStyles, watchTask)
