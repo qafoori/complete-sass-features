16364a39d43691c0a8c5bd3e0e7bf278a9e940e8
qafoori <qafoori.hr@gmail.com>
2022-02-12 21:03:45 +0330

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const stylesSource = 'src/styles/**/*.scss'
+
+const compileStyles = () => src(stylesSource).pipe(sass()).pipe(dest('css'))
+
+const watchTask = () => {
+  watch([stylesSource], compileStyles)
+}
+
+exports.default = series(compileStyles, watchTask)
