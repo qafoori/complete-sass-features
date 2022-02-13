848704cab8e89c75abac60170c68a50110952af6
qafoori.hr <qafoori.hr@gmail.com>
2022-02-14 01:28:58 +0330

    completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const { src, dest, watch, series } = require('gulp')
+const purgeCSS = require('gulp-purgecss')
 const sass = require('gulp-sass')(require('sass'))
+
 const stylesSource = 'src/styles/**/*.scss'
+const htmlSource = 'src/**/*.html'
 
-const compileStyles = () => src(stylesSource).pipe(sass()).pipe(dest('css'))
+const compileStyles = () =>
+  src(stylesSource)
+    .pipe(sass())
+    .pipe(purgeCSS({ content: [htmlSource] }))
+    .pipe(dest('css'))
 
-const watchTask = () => watch([stylesSource], compileStyles)
+const watchTask = () => watch([stylesSource, htmlSource], compileStyles)
 
 exports.default = series(compileStyles, watchTask)
