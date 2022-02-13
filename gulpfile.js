d6fb951223e9b6053af4736f0eff4515f6c0e826
qafoori.hr <qafoori.hr@gmail.com>
2022-02-14 01:42:47 +0330

    adding sourcemap and clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const { src, dest, watch, series } = require('gulp')
 const purgeCSS = require('gulp-purgecss')
+const cleanCSS = require('gulp-clean-css')
+const sourcemaps = require('gulp-sourcemaps')
 const sass = require('gulp-sass')(require('sass'))
 
 const stylesSource = 'src/styles/**/*.scss'
@@ -7,8 +9,11 @@ const htmlSource = 'src/**/*.html'
 
 const compileStyles = () =>
   src(stylesSource)
+    .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(purgeCSS({ content: [htmlSource] }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(sourcemaps.write())
     .pipe(dest('css'))
 
 const watchTask = () => watch([stylesSource, htmlSource], compileStyles)
